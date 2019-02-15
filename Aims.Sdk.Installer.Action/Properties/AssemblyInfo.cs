8b8c664043d26b1e331c65cbf35eb3f659097295
Tigran <tigran.martirosyan@aimsinnovation.com>
2019-02-15 18:09:53 +0100

    BIZ-5151: Add symbolic versioning to the HTTP agent

--- Aims.Sdk.Installer.Action/Properties/AssemblyInfo.cs
+++ Aims.Sdk.Installer.Action/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aims.Scom.Installer.Action")]
+[assembly: AssemblyTitle("Aims.HTTP.Installer.Action")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aims.Scom.Installer.Action")]
+[assembly: AssemblyProduct("Aims.HTTP.Installer.Action")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
